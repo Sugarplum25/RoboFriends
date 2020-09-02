9778fb1ff784162b05b56a01c9a88c3ebd71025d
Sugarplum25 <sugarplamka@gmail.com>
2020-09-02 16:07:09 +0400

    add font, change some styles

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -3,8 +3,9 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
     const cardsArray = robots.map((user, i) => <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>);
+    console.log(cardsArray);
     return (
-        <div>
+        <div className="flex flex-wrap justify-center">
             {cardsArray}
         </div>
     );
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <h1 className='green tc'>RoboFriends</h1>
+        <h1>RoboFriends</h1>
     );
 };
 
