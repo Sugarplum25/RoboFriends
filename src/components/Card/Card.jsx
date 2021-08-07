9a429be79c19f82c2706eaa781ab209db76e6ed4
Sugarplum25 <Sugarplamka@gmail.com>
2021-08-07 18:14:52 +0300

    refactor: change class Card componento to functional with hook

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cn from 'classnames';
 import './card.css';
 
-class Card extends React.Component {
-    state = {
-        color: false,
-    };
-    changeColor = () => {
-        this.setState(({ color }) => ({color: !color}));
-    }
-    
-    render() {
-        const { id, name, email } = this.props;
-        const cardClasses = cn('card', {'color': this.state.color});
-        return (
-            <div className={cardClasses} onClick={this.changeColor}>
-                <div>
-                    <img src={`https://robohash.org/${id}?size=100x100`} alt="robot">
-                    </img>
-                </div>
-                <div>
-                    <h2 className="card-title">{name}</h2>
-                    <p className="card-text">{email}</p>
-                </div>
-            </div>
-        );
-    }
+
+const Card = ({id, name, email}) => {
+    const [color, setColor] = useState(false);
+    const cardClasses = cn('card', {color});
+    const changeColor = () => setColor(!color);
+    return (
+        <div className={cardClasses} onClick={changeColor}>
+        <div>
+            <img src={`https://robohash.org/${id}?size=100x100`} alt="robot">
+            </img>
+        </div>
+        <div>
+            <h2 className="card-title">{name}</h2>
+            <p className="card-text">{email}</p>
+        </div>
+    </div>
+    )
 };
 
 export default Card;
